e41814641de5de7730757839e2425a3c69465461
HungDuyVu <vuhung.vtq@gmail.com>
2023-04-24 20:39:16 +0700

    video10 nav-ejs loop data

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -4,21 +4,21 @@ let getHomepage = (req, res) => {
      // logic
      let data = [];
      connection.query(
-          'SELECT * FROM `user`',
+          'SELECT * FROM `user` ',
           function(err, results, fields) {
-               results.map((row) => { data.push({
-                    id: row.id,
-                    email: row.email,
-                    address: row.address,
-                    firstName: row.firstName,
-                    lastName: row.lastName
-               })
+               results.map((row) => { 
+                    data.push({
+                         id: row.id,
+                         email: row.email,
+                         address: row.address,
+                         firstName: row.firstName,
+                         lastName: row.lastName
+                    })
                });
-               return res.render('test/index.ejs', {dataUser: JSON.stringify(data)});
+               // file muon truyen, object muon truyen
+               return res.render('index.ejs', { dataUser: data, test: 'abc string test' });
           }
      );
-     
-     
 }
 
 module.exports = {
