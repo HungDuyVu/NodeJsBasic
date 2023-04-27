e29468b5558104ed5dc40318b14594d227305528
HungDuyVu <vuhung.vtq@gmail.com>
2023-04-27 09:09:48 +0700

    video14 restfull API

--- a.md
+++ a.md
@@ -26,3 +26,7 @@ Tham so tren router:
      Route path: /users/:userId/books/:bookId
      Request URL: http://localhost:3000/users/34/books/8989
      req.params: { "userId": "34", "bookId": "8989" }
+
+
+Buoc 7: RESTfull APT     
+     cài đặt postman API
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import configViewEngine from './configs/viewEngine';
 import initWebRoute from './route/web';
+import initAPIRoute from './route/api';
 // import connection from './configs/connectDB';
 
 require('dotenv').config();
@@ -18,6 +19,8 @@ configViewEngine(app);
 // init web routes
 initWebRoute(app);
 
+// init api route
+initAPIRoute(app);
 
 
 app.listen(port, () => {
